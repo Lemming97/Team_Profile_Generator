346a0440057ec9f8bc8a60e855d8d0544d74817c
Lindsey Martin <martinlindsey87@yahoo.com>
2022-06-14 00:18:44 -0700

    updated icons and sizing

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -5,7 +5,7 @@ const generateManager = (manager) => {
         <div class="card h-100">
             <div class="card-header">
                 <h3>${manager.name}</h3>
-                <h4>Manager</h4><i class="material-icons">content_paste</i>
+                <h4>Manager</h4><i class="material-icons">person_pin</i>
             </div>
             <div class="card-body">
                 <p class="id">ID: ${manager.id}</p>
@@ -24,7 +24,7 @@ const generateEngineer = (engineer) => {
         <div class="card h-100">
             <div class="card-header">
                 <h3>${engineer.name}</h3>
-                <h4>Engineer</h4><i class="material-icons">laptop_mac</i>
+                <h4>Engineer</h4><i class="material-icons">developer_mode</i>
             </div>
             <div class="card-body">
                 <p class="id">ID: ${engineer.id}</p>
@@ -43,7 +43,7 @@ const generateIntern = (intern) => {
         <div class="card h-100">
             <div class="card-header">
                 <h3>${intern.name}</h3>
-                <h4>Intern</h4><i class="material-icons">assignment_ind</i>
+                <h4>Intern</h4><i class="material-icons">school</i>
             </div>
             <div class="card-body">
                 <p class="id">ID: ${intern.id}</p>
@@ -52,6 +52,9 @@ const generateIntern = (intern) => {
             </div>
     </div>
 </div>
+<span class="material-symbols-outlined">
+school
+</span>
     `
 };
 
