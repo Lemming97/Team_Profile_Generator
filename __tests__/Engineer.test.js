f98793e8ecf225c0aea0feeb44c17f66d56dea86
Lindsey Martin <martinlindsey87@yahoo.com>
2022-06-13 17:30:17 -0700

    updated layout

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,23 @@
+// require Engineer constructor 
+const Engineer = require('../lib/Engineer');
+
+// creating engineer object  
+test('creates an Engineer object', () => {
+    const engineer = new Engineer('Lindsey', 90, 'martinlindsey87@yahoo.com', 'Lemming97');
+    
+    expect(engineer.github) .toEqual(expect.any(String));
+});
+
+// gets github from getGithub()
+test('gets engineer github value', () => {
+    const engineer = new Engineer('Lindsey', 90, 'martinlindsey87@yahoo.com', 'Lemming97');
+
+    expect(engineer.getGithub()).toEqual(expect.stringContaining(engineer.github.toString()));
+});
+
+// gets role from getRole() 
+test('gets role of employee', () => {
+    const engineer = new Engineer('Lindsey', 90, 'martinlindsey87@yahoo.com', 'Lemming97');
+
+    expect(engineer.getRole()).toEqual("Engineer");
+});
---
+++ __tests__/Manager.test.js
@@ -0,0 +1,16 @@
+// require Manager constructor 
+const Manager = require('../lib/Manager');
+
+// creating manager object  
+test('creates an Manager object', () => {
+    const manager = new Manager('Lindsey', 90, 'martinlindsey87@yahoo.com', 4);
+    
+    expect(manager.officeNumber).toEqual(expect.any(Number));
+});
+
+// gets role from getRole()
+test('gets role of employee', () => {
+    const manager = new Manager('Lindsey', 90, 'martinlindsey87@yahoo.com');
+
+    expect(manager.getRole()).toEqual("Manager");
+}); 
